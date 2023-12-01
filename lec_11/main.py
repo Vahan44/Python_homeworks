30b844f45b4de79316d6fe39d8a0f0249a3e6de3
Name Surname <vahan.balyan.44@gmail.com>
2023-12-01 23:49:47 +0400

    lec_12

--- lec_11/main.py
+++ lec_11/main.py
@@ -1,81 +1,15 @@
-import requests
-import json
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
 
+nterms = 39
 
-# GET
-response = requests.get('https://jsonplaceholder.typicode.com/posts')
-if response.status_code == 200:
-    posts = response.json()
-    print('ALL POSTS')
-    print(json.dumps(posts, indent=2))
-
-    byTitle = [post for post in posts if len(post['title'].split()) <= 6]
-    print('Posts filtered by title')
-    print(json.dumps(byTitle, indent=2))
-
-    byBody = [post for post in posts if len(post['body'].split('\n')) <= 3]
-    print('Posts filtered by body')
-    print(json.dumps(byBody, indent=2))
-
-
-# GET BY ID
-response = requests.get('https://jsonplaceholder.typicode.com/posts/2')
-if response.status_code == 200:
-    post = response.json()
-    print('POST #2')
-    print(json.dumps(post, indent=2))
-
-# GET COMMENTS OF POST 2
-response = requests.get('https://jsonplaceholder.typicode.com/posts/2/comments')
-if response.status_code == 200:
-    comments = response.json()
-    print('POST #2 COMMENTS')
-    print(json.dumps(comments, indent=2))
-
-
-# POST
-response = requests.post(
-    'https://jsonplaceholder.typicode.com/posts',
-    json={
-        'title': 'NEW post',
-        'body': 'Post body',
-        'userId': 1
-    }
-)
-if response.status_code == 201:
-    newPost = response.json()
-    print('POST ADDED')
-    print(json.dumps(newPost, indent=2))
-
-# PUT
-response = requests.put(
-    'https://jsonplaceholder.typicode.com/posts/1',
-    json={
-        'title': 'Change title',
-        'body': 'post body.',
-        'userId': 1
-    }
-)
-if response.status_code == 200:
-    updatedPost = response.json()
-    print('UPDATED POST 1')
-    print(json.dumps(updatedPost, indent=2))
-
-
-# PATCH
-response = requests.patch(
-    'https://jsonplaceholder.typicode.com/posts/1',
-    json={
-        "title": "Updated Title"
-    }
-)
-if response.status_code == 200:
-    updatedPost = response.json()
-    print('PARTIALLY UPDATED POST #1')
-    print(json.dumps(updatedPost, indent=2))
-
-
-# DELETE
-response = requests.delete('https://jsonplaceholder.typicode.com/posts/1')
-if response.status_code == 200:
-    print('POST #1 DELETED')
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
